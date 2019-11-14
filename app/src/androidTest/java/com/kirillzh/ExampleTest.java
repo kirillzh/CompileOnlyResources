c58bb48ce0c7410e631e3fd17a29e69b7629bccd
Kirill Zhukov <zhukov@squareup.com>
2019-11-13 16:24:48 -0800

    set up demo project

---
+++ app/src/androidTest/java/com/kirillzh/ExampleTest.java
@@ -0,0 +1,13 @@
+package com.kirillzh;
+
+import com.kirillzh.featurerobots.FeatureRobot;
+
+import org.junit.Test;
+
+public class ExampleTest {
+
+    @Test
+    public void testSomething() {
+        new FeatureRobot().clickSomething();
+    }
+}
---
+++ feature-robots/src/main/java/com/kirillzh/featurerobots/FeatureRobot.java
@@ -0,0 +1,7 @@
+package com.kirillzh.featurerobots;
+
+public class FeatureRobot {
+    public void clickSomething() {
+        int res = R.string.something;
+    }
+}
